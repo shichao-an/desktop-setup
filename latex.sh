378eafd4b38e499a4735e774d2c444a1f3e945eb
Shichao An <shichao.an@nyu.edu>
2014-02-28 18:37:17 -0500

    Updated texlive packages

--- latex.sh
+++ latex.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 yum -y install texlive texlive-greektex texlive-greek-fontenc texlive-greek-inputenc \
-    texlive-cbfonts texlive-cbfonts-fd texlive-epigrafica
-
+    texlive-cbfonts texlive-cbfonts-fd texlive-epigrafica texlive-hyphenat \
+    texlive-endnotes texlive-titlesec tex-live-lipsum
