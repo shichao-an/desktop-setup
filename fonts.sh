2823446916f60798bfda309f2b85fefb0a0cdf3d
Shichao An <shichao.an@nyu.edu>
2014-03-04 18:57:03 -0500

    Added truetype install option

--- fonts.sh
+++ fonts.sh
@@ -8,3 +8,13 @@ yum -y install freetype-infinality fontconfig-infinality libXft-infinality
 
 # Install fonts
 yum -y install wqy-microhei-fonts 
+
+# Install Microsfot TrueType fonts
+if [ "$1" = "msttcorefonts"]
+then
+    yum -y install rpm-build cabextract ttmkfdir
+    wget http://corefonts.sourceforge.net/msttcorefonts-2.5-1.spec
+    rpmbuild -bb msttcorefonts-2.5-1.spec
+    rpm -ivh $HOME/rpmbuild/RPMS/noarch/msttcorefonts-2.5-1.noarch.rpm
+    fc-cache -f -v
+fi
