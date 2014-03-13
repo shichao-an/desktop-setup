08fe6070db4e59c3671886b086247131c040d13e
Shichao An <shichao.an@nyu.edu>
2014-03-13 14:37:45 -0400

    Added opensans font

--- latex.sh
+++ latex.sh
@@ -2,4 +2,4 @@
 
 yum -y install texlive texlive-greektex texlive-greek-fontenc texlive-greek-inputenc \
     texlive-cbfonts texlive-cbfonts-fd texlive-epigrafica texlive-hyphenat \
-    texlive-endnotes texlive-titlesec texlive-lipsum
+    texlive-endnotes texlive-titlesec texlive-lipsum texlive-opensans
