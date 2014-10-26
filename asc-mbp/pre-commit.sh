5687a7d28c108b36ca1f9749290e3c3f879a4127
Shichao An <shichao.an@nyu.edu>
2014-10-25 18:01:17 -0700

    Fixed

--- asc-mbp/pre-commit.sh
+++ asc-mbp/pre-commit.sh
@@ -5,4 +5,4 @@ rsync ~/.bash_aliases ~/.bash_profile ~/.bashrc ~/.bash_completion .
 rsync ~/.ssh/config .ssh/
 rsync ~/.sleep ~/.wakeup .
 rsync -r --exclude '*.swp' ~/bin .
-rsync "$local_etc_dir/dnsmasq.conf" "$local_etc_dir/dnsmasq.hosts" .
+rsync "$local_etc_dir/dnsmasq.conf" "$local_etc_dir/dnsmasq.hosts" etc
