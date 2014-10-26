a03319bbbcd8aaaf63205ff74a9f98d5b9a6cda2
Shichao An <shichao.an@nyu.edu>
2014-10-25 17:58:57 -0700

    Added pre-commit for dnsmasq

--- asc-mbp/pre-commit.sh
+++ asc-mbp/pre-commit.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+local_etc_dir="/usr/local/etc"
 rsync ~/.bash_aliases ~/.bash_profile ~/.bashrc ~/.bash_completion .
 rsync ~/.ssh/config .ssh/
 rsync ~/.sleep ~/.wakeup .
 rsync -r --exclude '*.swp' ~/bin .
+rsync "$local_etc_dir/dnsmasq.conf" "$local_etc_dir/dnsmasq.hosts" .
